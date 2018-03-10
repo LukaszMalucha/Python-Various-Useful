668d10e4a77200838319bd3c469802c1cefae678
Lukasz_Malucha <lucasmalucha@gmail.com>
2018-03-10 16:07:32 +0000

    Initial commit

---
+++ twitter_intro.py
@@ -0,0 +1,20 @@
+import json
+import tweepy
+from tweepy import OAuthHandler
+
+
+CONSUMER_KEY = 'JdtQ9Bo5WdOfwmZwZCaa6xUq7'
+CONSUMER_SECRET = 'oNoZDCtojF076qj8FB9XizZapj8GImTzkrPchWz9u8lqEBBjVA'
+OAUTH_TOKEN = '972493075317776385-F6SjPPjJgRs2tc5ktmFRkWiNBBHaAPh'
+OAUTH_TOKEN_SECRET = '03zatnZGuWrMzpsxZAngfWNjoYtpT9OW1TqPBMk7IbBHD'
+
+auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)                      
+auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)                  ## for authorization
+
+api = tweepy.API(auth)                                                  ## Connect to data
+
+DUB_WOE_ID = 560743                                                     ## Dublin WhereOnEarth
+
+dub_trends = api.trends_place(DUB_WOE_ID)                               ## assign Dublin trends to variable
+
+print(json.dumps(dub_trends, indent=1))                                 ## Print results to console
