037f04f1772dcd15416c5192eaf1e516bace51bf
Lukasz_Malucha <lucasmalucha@gmail.com>
2018-03-10 16:37:58 +0000

    Final twitter app commit

--- twitter_intro.py
+++ twitter_intro.py
@@ -14,7 +14,17 @@ auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)                  ## for a
 api = tweepy.API(auth)                                                  ## Connect to data
 
 DUB_WOE_ID = 560743                                                     ## Dublin WhereOnEarth
+LON_WOE_ID = 44418 
 
 dub_trends = api.trends_place(DUB_WOE_ID)                               ## assign Dublin trends to variable
+lon_trends = api.trends_place(LON_WOE_ID)
 
-print(json.dumps(dub_trends, indent=1))                                 ## Print results to console
+dub_trends_set = set([trend['name']                                     ## create sets for lon and dub
+                    for trend in dub_trends[0]['trends']])
+lon_trends_set = set([trend['name']
+                    for trend in lon_trends[0]['trends']])         
+                    
+common_trends = set.intersection(dub_trends_set, lon_trends_set)        ## common trends for both cities          
+
+
+print(common_trends)
