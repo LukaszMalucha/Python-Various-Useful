bb6f765ce283f4e66140cf879fdf74e8744c9d3b
Lukasz_Malucha <lucasmalucha@gmail.com>
2019-01-07 19:37:47 +0000

    iter

--- review/refresh_11_iterables.py
+++ review/refresh_11_iterables.py
@@ -98,13 +98,216 @@ enumerate(l1)
 open('asd.csv')
 dictionary.keys() /keys /values
 
+## list
 with open('cars.csv') as f:
     l = f.readlines()
 
 
+############################################################## Sorting Iterables
+
+import random
+
+random.seed(0)
+
+for _ in range(10):
+    print(random.randint(1,10))
+    
+    
+class RandomInts:
+    def __init__(self, length, *, seed=0, lower=0, upper=10):
+        self.length = length
+        self.seed = seed
+        self.lower = lower
+        self.upper = upper
+        
+    def __length__(self):
+        return self.length
+    
+    def __iter__(self):
+        return self.RandomIterator(self.length, seed = self.seed, lower = self.lower, upper = self.upper)
+        
+        
+        
+    class RandomIterator:     
+        def __init__(self, length, *, seed, lower, upper):
+            self.length = length
+            self.lower = lower
+            self.upper = upper
+            self.num_requests = 0
+            random.seed(seed)
+            
+        def __iter__(self):
+            return self
+        
+        def __next__(self):
+            if self.num_requests >= self.length:
+                raise StopIteration
+            else:
+                result = random.randint(self.lower, self.upper)
+        
+            
+randoms = RandomInts(10)        
+
+for num in randoms:
+    print(num)
+        
+        
+############################################################## Iterate Callables 
+
+
+def counter():
+    i = 0
+    
+    def inc():
+        nonlocal i
+        i += 1
+        return i
+    
+    return inc
+
+
+class CounterIterator:
+    def __init__(self, counter_callable, sentinel):
+        self.counter_callable = counter_callable
+        self.sentinel = sentinel
+        
+        
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        result = self.counter_callable()
+        if result == self.sentinel:
+            raise StopIteration
+        else:
+            return result
+        
+        
+
+cnt = counter()
+
+cnt_iter = CounterIterator(cnt)
+
+for _ in range(5):
+    print(next(cnt_iter))        
+        
+        
+        
+############################################################## Reverse Iteration         
+        
+## Copy sequence
+for item in seq[::-1]:
+    print(item)
+        
+        
+for i in range(len(seq)):
+    print(seq[len(seq)] - i - 1)        
+    
+for i in range(len(seq)-1, -1, -1):
+    print(seq[i])    
+        
+        
+for item in reversed(seq):
+    print(item)        
+        
+###################################################################### Card Deck   
+
+_SUITS = ('Spades', 'Hearts', 'Diamonds', 'Clubs') 
+_RANKS = tuple(range(2,11)) + tuple('JQKA')
+
+from collections import namedtuple
+
+Card = namedtuple('Card', 'rank suit')
+
+class CardDeck:
+    def __init__(self):
+        self.length = len(_SUITS) * len(_RANKS)
+
+    def __len__(self):
+        return self.length
+    
+    def __iter__(self):
+        return self.CardDeckIterator(self.length)
+    
+    def __reversed__(self):
+        return self.CardDeckIterator(self.length, reverse=True)
+    
+    class CardDeckIterator:
+        def __init__(self, length, reverse=False):
+            self.length = length
+            self.reverse = reverse
+            self.i = 0
+            
+        def __iter__(self):
+            return self
+        
+        def __next__(self):
+            if self.i >= self.length:
+                raise StopIteration
+            else:
+                if self.reverse:
+                    index = self.length - 1 - self.i
+                else:
+                    index = self.i
+                suit = _SUITS[index // len(_RANKS)]
+                rank = _RANKS[index % len(_RANKS)]
+                self.i = 0
+                return Card(rank, suit)
+                
+            
+
+deck = reversed(CardDeck())
+
+for card in deck:
+    print(card)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+        
+        
+        
+        
+        
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 
