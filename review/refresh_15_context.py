76ffed7c564e7b3dbfd347a38ce929d5e2c7b1ae
Lukasz_Malucha <lucasmalucha@gmail.com>
2019-01-09 19:15:39 +0000

    15

--- review/refresh_15_context.py
+++ review/refresh_15_context.py
@@ -1,7 +1,86 @@
 # -*- coding: utf-8 -*-
 
 
-################################################################ Context manager
+################################################################ Context Manager
 
 with open('test.txt', 'r') as f:
     print(f.readlines())
+    
+    
+## issue with returning lazy iterator    
+import csv
+
+def read_data():
+    with open('file.txt') as f:
+        return csv.reader(f, delimiter = ',')
+    
+reader = read_data()    
+    
+for row in reader:
+    print(row)    
+    
+## fix
+def read_data():
+    with open('file.txt') as f:
+        yield from csv.reader(f, delimiter = ',')    
+
+reader = read_data() 
+
+for row in reader:
+    print(row)    
+        
+## fix 2 (memory hungry)  
+
+def read_data():
+    with open('file.txt') as f:
+        return list(csv.reader(f, delimiter = ','))
+    
+reader = read_data()    
+    
+for row in reader:
+    print(row)  
+    
+#################################################### Generator + context manager  
+
+def my_gen():
+    try:
+        print('create context')
+        yield [1,2,3,4]
+    finally:
+        print('exiting context')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
